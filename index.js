c84231b059f8c7954b1fc9bb8da8e1e157a6329a
Kiran Amin <123891544+kiranamin05@users.noreply.github.com>
2023-02-10 00:05:03 +0530

    Initial Todo App/habit tracker -
    Initial Todo App/habit tracker -

---
+++ index.js
@@ -0,0 +1,17 @@
+// DataTypes Practice
+
+console.log( 10 + "20");
+// 9 - "5"
+// console.log( 9 - "5"); //bug
+// "Java" + "Script"
+// console.log( "Java "+ "Script");
+// " " + " "
+// console.log( " " + 0);
+// " " + 0
+// "vinod" - "thapa"
+// true + true
+// true + false
+// false + true
+// false - true
+
+
